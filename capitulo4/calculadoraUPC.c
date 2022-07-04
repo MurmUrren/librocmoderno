276cba9999866ee2fbd5a1d5ca62f53d4f2f3c3d
MurmUrren <igorspopovich@hotmail.com>
2022-07-03 19:06:34 -0700

    actualizacion

--- capitulo4/calculadoraUPC.c
+++ capitulo4/calculadoraUPC.c
@@ -19,7 +19,7 @@ int main(void)
     second_sum = i1 + i3 + i5 + j2 + j4;
     total = 3 * first_sum + second_sum;
     
-    printf ("Check digit: %d\n", 9 - ((total - 1) % 10)) ;
+    printf ("Check digit: %d\n", 9 - ((total - 1) % 10));
 
     return 0;
 }
